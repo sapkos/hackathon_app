ff311d0f67bb000a3f0d7cdb899e48ec7d79f1dd
ssapkowski <szymon.sapkowski@lovo.pl>
2018-05-27 06:20:22 +0200

    new maps fastfood i medyczne

--- app.py
+++ app.py
@@ -25,13 +25,6 @@ conn_params = dict(host='85.194.245.31',
                    password='!TajemniczaTajemnica7'
                    )
 
-grid_centers = pd.read_csv('grid_centers.csv', sep=';')
-
-def get_grid_center(grid):
-   return grid_centers[grid_centers['eurogrid_0250']==grid].values[0]
-
-pred = pd.read_csv('preds_with_center.csv')
-pred['digits'] = np.digitize(pred['pred'], np.linspace(0,pred['pred'].max(),10))
 
 app = dash.Dash(__name__)
 
@@ -39,9 +32,10 @@ app.css.append_css({'external_url': 'https://github.com/sapkos/hackathon_app/blo
 app.css.append_css({'external_url': 'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css'})
 
 app.config.supress_callback_exceptions=True
-categories = dict(Cat_1=['a','b','c'], Cat_2=['d','e','f'], Cat_3=['g','h','e'])
+categories = dict(Cat_1=['Restauracje','FastFood','c'], Cat_2=['uslugi_medyczne','e','f'], Cat_3=['Przedszkola','h','e'])
 
 n_clicks = [0,0,0]
+colors = ['#ff5454', '#ffff54', '#eeff00', '#8fff54', '#00ff19']
 
 app.layout = html.Div([
       
@@ -78,30 +72,7 @@ app.layout = html.Div([
          html.Div(id='subcat-dropdown', style={'width':'50%'}),
          dcc.Graph(
              id = "mapbox",
-             figure={
-                 "data": [go.Scattermapbox(
-                 lat=pred[pred['digits']==i]['grid_y'].values,
-                 lon=pred[pred['digits']==i]['grid_x'].values,
-                 mode='markers',
-                 marker=dict(
-                     size=20,
-                     opacity=i/(5*pred['digits'].max()),
-                     color='rgb(255, 153, 0)'
-                 )
-                 ) for i in range(1, pred['digits'].max() + 1)],
-                 "layout": dict(
-                     autosize = True,
-                     hovermode = "closest",
-                     margin = dict(l = 0, r = 0, t = 0, b = 0),
-                     mapbox = dict(
-                         accesstoken = 'pk.eyJ1Ijoic3p5bWVrazk1IiwiYSI6ImNqZ3BqcHN6ZjJ0dngycW84OGZnaHdoaGMifQ.xVWsCrwXz-6qYX5MYJjmoQ',
-                         bearing = 0,
-                         style = "light",
-                         pitch = 0,
-                         zoom = 5
-                     )
-                 )
-             },
+             figure={},
              style = {"height": "100vh"}
       )
    ], style={'padding': '0px 10px 15px 10px',
@@ -123,8 +94,45 @@ def update_output(cat1, cat2, cat3):
       clicked = 'Cat_3'
    else:
       return None
-   return dcc.Dropdown(options=[{'label': j, 'value': j} for j in categories[clicked]])
+   return dcc.Dropdown(id='subcat-dropdown-dropdown', 
+                       options=[{'label': j, 'value': j} for j in categories[clicked]])
+
    
+@app.callback(Output('mapbox', 'figure'),
+              [Input('subcat-dropdown-dropdown', 'value')])
+def update_graph(category):
+   pred = pd.read_csv(category+'_xy.csv')
+   lin = [np.percentile(pred.pred.values, q) for q in np.linspace(0,100,5)]
+   figure = {
+                 "data": [go.Scattermapbox(
+                 lat=pred[pred['digits']==i]['grid_y'].values,
+                 lon=pred[pred['digits']==i]['grid_x'].values,
+                 mode='markers',
+                 marker=dict(
+                     size=30,
+                     opacity=i/(3*pred['digits'].max()),
+                     color=colors[i]
+                 ),
+                 text=lin[i]  
+                 ) for i in range(0, 5)],
+                 "layout": dict(
+                     autosize = True,
+                     hovermode = "closest",
+                     margin = dict(l = 0, r = 0, t = 0, b = 0),
+                     mapbox = dict(
+                         accesstoken = 'pk.eyJ1Ijoic3p5bWVrazk1IiwiYSI6ImNqZ3BqcHN6ZjJ0dngycW84OGZnaHdoaGMifQ.xVWsCrwXz-6qYX5MYJjmoQ',
+                         bearing = 0,
+                         pitch = 0,
+                         zoom=11,
+                         center=dict(
+                               lat=50.062204,
+                               lon=19.938160
+                               )
+                     )
+                 )
+            }
+   return figure 
+
 
 
 if __name__ == '__main__':
